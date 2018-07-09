4b0175eff950e768f24c49984a581d40ee348f82
Karine Coutinho <ka.coutinho@hotmail.com>
2018-07-09 17:31:24 -0300

    versão 002

--- main.js
+++ main.js
@@ -1,16 +1,34 @@
-var submitTwitter = document.querySelector ('button');
-submitTwitter.onclick = function(event){
-    var inputTwitter = document.querySelector('input');
-    var backgroundImgContainer = document.getElementById('backgroundImgContainer'); 
-    var tweets = document.createElement('p');
-    tweets.textContent = inputTwitter.value;
-    backgroundImgContainer.appendChild(tweets);
-    event.preventDefault();
+
+var submitTwitter = document.querySelector ('.tweetButton');
+var inputTwitter = document.querySelector('.tweetText');
+var twitterForm = document.querySelector('.tweetForm');
+
+
+//contando caracteres
+var counting = document.querySelector('#counter');
+inputTwitter.addEventListener('keyup', countCharacters);
+
+function countCharacters(){ 
+    var inputTwitterLength = inputTwitter.value.length;
+    var maxCharacters =140;
+    var characteresLeft = maxCharacters - inputTwitterLength;
+    counting.innerHTML = characteresLeft;
+    if(inputTwitterLength > 0){
+        submitTwitter.disabled =false;
+    } 
 }
 
-
-
-
+//publicando os Twitter
+submitTwitter.onclick = function(event){
+    var twitterPage = document.getElementById('twitterPage'); 
+    var tweets = document.createElement('p');
+    tweets.textContent = inputTwitter.value;
+    twitterPage.appendChild(tweets);
+    event.preventDefault(); 
+    twitterForm.reset();
+    counting.innerHTML = '140';
+    submitTwitter.disabled = true;
+}
 
 
 
